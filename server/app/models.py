bd1a5504e72707e9d2ce7c5a9805ff1b3c7c047a
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-07-16 21:06:37 +0300

    done

--- server/app/models.py
+++ server/app/models.py
@@ -1,21 +1,44 @@
 from app import db
-from datetime import datetime
+# from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+user_clubs = db.Table('user_clubs',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('club_id', db.Integer, db.ForeignKey('clubs.id'), primary_key=True)
+)
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    profile_pic = db.Column(db.String(200))
-    bio = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(10000))
+    # profile_pic = db.Column(db.String(200))
+    # bio = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-    # Relationships
-    posts = db.relationship('Post', back_populates='author', lazy=True)
-    comments = db.relationship('Comment', back_populates='author', lazy=True)
-    ratings = db.relationship('Rating', back_populates='author', lazy=True)
-    clubs = db.relationship('Club', secondary='user_clubs', back_populates='members')
+    posts = db.relationship('Post', back_populates='author', lazy='dynamic')
+    comments = db.relationship('Comment', back_populates='author', lazy='dynamic')
+    ratings = db.relationship('Rating', back_populates='author', lazy='dynamic')
+    clubs = db.relationship('Club', secondary=user_clubs, back_populates='members')
+    watched_movies = db.relationship('WatchedMovie', back_populates='user')
+    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
     __tablename__ = 'movies'
@@ -25,11 +48,9 @@ class Movie(db.Model):
     overview = db.Column(db.Text)
     release_date = db.Column(db.Date)
     poster_path = db.Column(db.String(200))
-    
-    # Relationships
-    posts = db.relationship('Post', back_populates='movie', lazy=True, foreign_keys='Post.movie_id')
-    ratings = db.relationship('Rating', back_populates='movie', lazy=True, foreign_keys='Rating.movie_id')
-    comments = db.relationship('Comment', back_populates='movie', lazy=True, foreign_keys='Comment.movie_id')
+
+    posts = db.relationship('Post', back_populates='movie', lazy='dynamic')
+    watched_by = db.relationship('WatchedMovie', back_populates='movie')
 
 class TVShow(db.Model):
     __tablename__ = 'tv_shows'
@@ -39,11 +60,8 @@ class TVShow(db.Model):
     overview = db.Column(db.Text)
     first_air_date = db.Column(db.Date)
     poster_path = db.Column(db.String(200))
-    
-    # Relationships
-    posts = db.relationship('Post', back_populates='tv_show', lazy=True, foreign_keys='Post.tv_show_id')
-    ratings = db.relationship('Rating', back_populates='tv_show', lazy=True, foreign_keys='Rating.tv_show_id')
-    comments = db.relationship('Comment', back_populates='tv_show', lazy=True, foreign_keys='Comment.tv_show_id')
+
+    posts = db.relationship('Post', back_populates='tv_show', lazy='dynamic')
 
 class Club(db.Model):
     __tablename__ = 'clubs'
@@ -51,65 +69,58 @@ class Club(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.Text)
     genre = db.Column(db.String(50))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    created_by_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    # Relationships
-    posts = db.relationship('Post', back_populates='club', lazy=True)
-    members = db.relationship('User', secondary='user_clubs', back_populates='clubs')
+    created_by = db.relationship('User', backref='created_clubs')
+    members = db.relationship('User', secondary=user_clubs, back_populates='clubs')
+    posts = db.relationship('Post', back_populates='club', lazy='dynamic')
 
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
     tv_show_id = db.Column(db.Integer, db.ForeignKey('tv_shows.id'))
     club_id = db.Column(db.Integer, db.ForeignKey('clubs.id'), nullable=False)
 
-    # Relationships
     author = db.relationship('User', back_populates='posts')
-    movie = db.relationship('Movie', back_populates='posts', foreign_keys=[movie_id])
-    tv_show = db.relationship('TVShow', back_populates='posts', foreign_keys=[tv_show_id])
+    movie = db.relationship('Movie', back_populates='posts')
+    tv_show = db.relationship('TVShow', back_populates='posts')
     club = db.relationship('Club', back_populates='posts')
-    comments = db.relationship('Comment', back_populates='post', lazy=True)
-    ratings = db.relationship('Rating', back_populates='post', lazy=True)
+    comments = db.relationship('Comment', back_populates='post', lazy='dynamic')
+    ratings = db.relationship('Rating', back_populates='post', lazy='dynamic')
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
-    tv_show_id = db.Column(db.Integer, db.ForeignKey('tv_shows.id'))
 
-    # Relationships
     author = db.relationship('User', back_populates='comments')
     post = db.relationship('Post', back_populates='comments')
-    movie = db.relationship('Movie', back_populates='comments', foreign_keys=[movie_id])
-    tv_show = db.relationship('TVShow', back_populates='comments', foreign_keys=[tv_show_id])
 
 class Rating(db.Model):
     __tablename__ = 'ratings'
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
-    tv_show_id = db.Column(db.Integer, db.ForeignKey('tv_shows.id'))
 
-    # Relationships
     author = db.relationship('User', back_populates='ratings')
     post = db.relationship('Post', back_populates='ratings')
-    movie = db.relationship('Movie', back_populates='ratings', foreign_keys=[movie_id])
-    tv_show = db.relationship('TVShow', back_populates='ratings', foreign_keys=[tv_show_id])
 
-# Association table for many-to-many relationship between Users and Clubs
-user_clubs = db.Table('user_clubs',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('club_id', db.Integer, db.ForeignKey('clubs.id'), primary_key=True)
-)
+class WatchedMovie(db.Model):
+    __tablename__ = 'watched_movies'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    watched_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    user = db.relationship('User', back_populates='watched_movies')
+    movie = db.relationship('Movie', back_populates='watched_by')
