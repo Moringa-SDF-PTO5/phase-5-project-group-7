bdd9e88007365d2a68133e5645d2f82add087982
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-07-20 20:56:56 +0300

    SECRET KEY

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     migrate.init_app(app, db)
 
     with app.app_context():
-        # from app import routes, models
+        from app import routes, models
         db.create_all()
 
     return app
