e698464e6a7e09a517243881a2801356724cc0ae
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-07-31 23:39:20 +0300

    updated config

--- server/app/config.py
+++ server/app/config.py
@@ -4,7 +4,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'default_url')
     ENVIRONMENT = os.getenv('APP_ENV', 'development')
     SECRET_KEY = 'tvmovieclub'
-    SESSION_TYPE = 'filesystem'
+    # SESSION_TYPE = 'filesystem'
 
     TMDB_BASE_URL = "https://api.themoviedb.org/3"
     TMDB_API_KEY = "e407ef47003f178633c02269142f8b86"
