716bde96b7ab03f8d7c9c4df3b1518224e775092
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-08-01 21:26:16 +0300

    postgres deploy

--- server/app/config.py
+++ server/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'default_url')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://tvguide:DchPLbl8WHpHq0rbmtIba68hz80p8obi@dpg-cqlsjul2ng1s73e88tsg-a.frankfurt-postgres.render.com/tvguide2_ah49')
     ENVIRONMENT = os.getenv('APP_ENV', 'development')
     SECRET_KEY = 'tvmovieclub'
     # SESSION_TYPE = 'filesystem'
