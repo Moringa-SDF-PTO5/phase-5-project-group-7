7948d303337343e7c0aca48c8b9b7e9ebe93cdf7
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-08-01 20:39:18 +0300

    api change to deployed url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:10000',
+    baseURL: 'https://phase-5-project-group-7.onrender.com',
 });
 
 // Add an interceptor to include the token in the headers if needed
