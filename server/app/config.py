95d67e26a4706c461363c0611e79ddc131b6318d
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-08-07 11:55:33 +0300

    config secret key and api fix

--- server/app/config.py
+++ server/app/config.py
@@ -5,10 +5,10 @@ class Config:
         'DATABASE_URL', 
         'postgresql://tvguide:DchPLbl8WHpHq0rbmtIba68hz80p8obi@dpg-cqlsjul2ng1s73e88tsg-a.frankfurt-postgres.render.com/tvguide2_ah49')
     ENVIRONMENT = os.getenv('APP_ENV', 'development')
-    SECRET_KEY = '.'
+    SECRET_KEY = 'tvmovieclub'
     TESTING = True
 
     TMDB_BASE_URL = "https://api.themoviedb.org/3"
-    TMDB_API_KEY = "."
+    TMDB_API_KEY = "e407ef47003f178633c02269142f8b86"
 
     
